ae07022d1eb44a346e6ffb40b4d95d2878a691cf
Siddhartha Singh <123258616+siddhartha038@users.noreply.github.com>
2024-01-29 19:15:38 +0530

    Add files via upload

---
+++ add.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+    int num1, num2;
+
+    std::cout << "Enter the first number: ";
+    std::cin >> num1;
+
+    std::cout << "Enter the second number: ";
+    std::cin >> num2;
+
+    int sum = num1 + num2;
+
+    std::cout << "Sum: " << sum << std::endl;
+
+    return 0;
+}
