0bb52061a21754a3a00f31440d392b23beae54a6
Siddhartha Singh <123258616+siddhartha038@users.noreply.github.com>
2024-04-09 22:44:41 +0530

    Add files via upload

---
+++ Problem7.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int reverse(int x) { 
+        long revx=0;
+        while(x!=0){
+            int ld=x%10;
+            revx=(revx*10)+ld;
+            x=x/10;
+        }
+        if(revx>INT_MAX || revx < INT_MIN ){
+            return 0;
+        }
+        
+        
+        else{
+            return revx;
+        }
+    }
+        
+};
