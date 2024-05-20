b28fc84fc0486d8299d691aefa61531e4fd0340f
Siddhartha Singh <123258616+siddhartha038@users.noreply.github.com>
2024-05-20 10:56:17 +0530

    Add fies via upload

---
+++ age.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int age;
+    cout<<"Enter your age:";
+    cin >>age;
+    if(age >= 18)
+     {
+        cout << "You are eligible to vote." << endl;
+     }
+    else
+     {
+        cout << "You are not eligible to vote." << endl;
+     }
+return 0;   
+}
