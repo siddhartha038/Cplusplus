066cc69e6024eea6fadba628b494e33941e1cb53
Siddhartha Singh <123258616+siddhartha038@users.noreply.github.com>
2024-05-21 10:22:08 +0530

    Add files via upload

---
+++ pattern.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() 
+{
+    int i , j;
+    for(i=0;i<4;i++)
+    {
+        for(j=0;j<4;j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+return 0;
+}
