6183d138a3ff6b56eb2a308a6dbf35f5f3cbe6f9
Siddhartha Singh <123258616+siddhartha038@users.noreply.github.com>
2024-06-03 21:06:30 +0530

    Add files via upload

---
+++ characterarray.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int arr[5] = {1,2,3,4,5};
+    char ch[6] = "abcde";
+
+    cout << arr << " is the address of the array" << endl;
+    cout << ch << " is the value of the character array" << endl;
+
+    char *c = &ch[0];
+    cout << c << endl;
+
+return 0;
+
+}
