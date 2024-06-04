22a968a2b666c5e0140c269381d6fa554c2a630f
Siddhartha Singh <123258616+siddhartha038@users.noreply.github.com>
2024-06-04 20:47:44 +0530

    Add files via upload

---
+++ doublepointer.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void update ( int **ptr)
+{
+    p = p + 1;
+}
+
+int main()
+{
+    int i =5;
+    int *ptr = &i;
+    int **ptr2 = &ptr;
+    
+    cout << endl << endl; 
+    cout << "Double pointer has been created" << endl;
+
+    cout << "printing ptr " << ptr << endl ;
+    cout << "address of ptr " << &ptr << endl;
+    cout << "printing value of p " << *ptr << endl;
+
+    cout << *ptr2 << endl;
+    cout << endl << endl;
+}
