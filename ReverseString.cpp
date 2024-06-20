eb554435045f2e5be204c45a164031ed36a39f9f
Siddhartha Singh <123258616+siddhartha038@users.noreply.github.com>
2024-06-20 17:44:40 +0530

    Add files via upload

---
+++ ReverseString.cpp
@@ -0,0 +1,17 @@
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int right = s.size()-1;
+        int left = 0;
+        while(left < right)
+        {
+            char temp =  s[left];
+            s[left] = s[right];
+            s[right] = temp;
+
+            left+=1;
+            right-=1;
+        }
+    }
+};
